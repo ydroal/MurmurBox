2e261e8e338a33f4036dd24df41a83d4ad84ea5a
ydroal <droal.yoko@gmail.com>
2024-10-14 16:59:21 +0200

    update: configure environment settings for production deployment

--- server/app.js
+++ server/app.js
@@ -3,14 +3,23 @@ const admin = require('firebase-admin');
 // const serviceAccount = require('./path/to/your-service-account-file.json');
 const cors = require('cors');
 const cookieParser = require('cookie-parser'); 
-const app = express()
 const dotenv = require('dotenv');
-const port = 3000
+const path = require('path');
+const app = express()
+
+// .envファイルから環境変数を読み込む
+dotenv.config(); 
+
+const port = process.env.PORT || 3000; // Herokuでポートは動的に設定される
 
 // CORSの設定
+const allowedOrigins = process.env.NODE_ENV === 'production'
+  ? ['https://murmurbox-7794a48dc19f.herokuapp.com']
+  : ['http://localhost:3000', 'http://localhost:5173'];
+
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'], // フロントエンドが動作するオリジンを指定
-  credentials: true // 認証情報を含むリクエストをサポート
+  origin: allowedOrigins,
+  credentials: true
 }));
 
 // JSONデータのパースを有効にする
@@ -19,13 +28,23 @@ app.use(express.json());
 // cookie-parserの設定（これによりreq.cookiesにアクセス可能になる）
 app.use(cookieParser());
 
+// 本番環境の場合のみ、dist/フォルダの静的ファイルを配信
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  // ルーティング設定の前に静的ファイル配信設定を追加
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+} else {
+  // 開発環境の場合は、クライアントアプリはViteの開発サーバーが管理
+  console.log('development mode. Not use static files from dist');
+}
+
 // ルーティング設定
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-// .envファイルから環境変数を読み込む
-dotenv.config(); 
 
 const routes = require('./routes'); // `routes.js`ファイルで定義されたルート群
 app.use('/api', routes); // APIエンドポイントのベースパスを指定
--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,5 +1,30 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/firebaseAdminKey.json');
+const fs = require('fs');
+const path = require('path');
+// const serviceAccount = require('./config/firebaseAdminKey.json');
+
+// Firebase Admin Keyの保存先ディレクトリ（開発環境）
+const firebaseKeyPath = path.resolve(__dirname, 'config', 'firebaseAdminKey.json');
+
+let serviceAccount;
+
+if (process.env.NODE_ENV === 'production') {
+  // 本番環境の場合、環境変数からFirebase Admin Keyを読み込む
+  if (process.env.FIREBASE_ADMIN_KEY_JSON) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY_JSON);
+  } else {
+    console.error('FIREBASE_ADMIN_KEY_JSON is not set in the environment variables.');
+    process.exit(1);
+  }
+} else {
+  // 開発環境の場合、ファイルからFirebase Admin Keyを読み込む
+  if (fs.existsSync(firebaseKeyPath)) {
+    serviceAccount = require(firebaseKeyPath);
+  } else {
+    console.error(`Firebase Admin Key file not found at: ${firebaseKeyPath}`);
+    process.exit(1);
+  }
+}
 
 // Firebase Admin SDKの初期化
 admin.initializeApp({
@@ -7,10 +32,6 @@ admin.initializeApp({
 });
 
 const db = admin.firestore(); 
-// テスト: `auth`が正しく初期化されているか確認
-// admin.auth().listUsers(1)
-//   .then(users => console.log('Firebase Auth Initialization OK', users))
-//   .catch(error => console.error('Error initializing Firebase Auth:', error));
 
 module.exports = {
   admin,
